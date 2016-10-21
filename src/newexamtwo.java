fe0dca9a6e542b6ee3517067be9bf18ea5e196a0
tangxiangru <871607149@qq.com>
2016-10-21 23:53:00 +0800

    init commit

---
+++ src/newexamtwo.java
@@ -0,0 +1,12 @@
+/**
+ * Created by tangxiangru on 16/10/21.
+ */
+public class newexamtwo {
+    public void print_Int array(int[] int_array)
+    {
+        java.util.Arrays.sort(int_array);
+        System.out.println("输出数组：");
+        for (int i=0;i<int_array.length;i++){
+            System.out.println(int_array[i]);
+    }
+}
